c12d1719f648dbd2b3e5cf67d07b1f1602bc3d6a
afonsolima@gmail.com <montecarlostec@gmail.com>
2023-05-09 18:00:26 -0300

    - implementacoes de classe - codigo com erro de compilacao

--- src/main/java/dio/padroes/service/impl/ClienteStrategyServiceImpl.java
+++ src/main/java/dio/padroes/service/impl/ClienteStrategyServiceImpl.java
@@ -1,24 +1,49 @@
 package dio.padroes.service.impl;
 
 import dio.padroes.model.Cliente;
+import dio.padroes.model.Endereco;
+import dio.padroes.repository.ClienteRepository;
+import dio.padroes.repository.EnderecoRepository;
 import dio.padroes.service.ClienteService;
+import dio.padroes.service.ViacepService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ClienteStrategyServiceImpl implements ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Autowired
+    private EnderecoRepository enderecoRepository;
+
+    @Autowired
+    private ViacepService viacepService;
+
     @Override
     public Iterable<Cliente> buscarTodos() {
-        return null;
+
+        return clienteRepository.findAll();
     }
 
     @Override
     public Cliente buscarPorId(Long id) {
-        return null;
+        Optional<Cliente> cliente = clienteRepository.findById(id) ;
+        return cliente.get();
     }
 
     @Override
     public void inserir(Cliente cliente) {
-
+        String cep = cliente.getEndereco().getCep();
+        Endereco endereco = enderecoRepository.findById(cep).orElseGet(() -> {
+            Endereco novoEndereco = viacepService.consultaCep(cep);
+            enderecoRepository.save(novoEndereco);
+            return novoEndereco;
+        });
+        cliente.setEndereco(endereco);
+        clienteRepository.save(cliente);
     }
 
     @Override
